594337cf72cc9c887ce3099f7dd732e2dc044159
Jason Halverson <jason.halverson@clearcapital.com>
2021-04-17 12:19:40 -0700

    Fixed backsash

--- keyboards/keebio/nyquist/keymaps/halversonjason/keymap.c
+++ keyboards/keebio/nyquist/keymaps/halversonjason/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_DEL, \
   KC_LWBK, KC_A,   KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  TD(TD_ESC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M, KC_COMM, KC_DOT,  TD(TD_SLSH_BSLS), KC_ENT , \
+  TD(TD_ESC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -91,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  f7  |  f8  |  f9  | f10  | f11  | f12  |      |   7  |   8  |   9  |      | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   4  |   5  |   6  |   0  |  \   |
+ * | Del  |      |      |      |      |      |      |   4  |   5  |   6  |   0  |     |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |  1   |  2   |  3   |   .  |Enter |
+ * |      |      |      |      |      |      |      |  1   |  2   |  3   |   \  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             | Raise| Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  _______,    KC_7,    KC_8,    KC_9, _______, KC_DEL, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,    KC_4,    KC_5,    KC_6,  KC_0, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_DOT, KC_ENT, \
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,    KC_4,    KC_5,    KC_6,  KC_0, _______, \
+  _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3,  KC_BSLS, KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
